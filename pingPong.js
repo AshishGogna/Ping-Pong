d268c976924a04f4d3fd8352bbb6724c59f492cc
Ashish <ashishgogna@Ashishs-MacBook-Pro.local>
2016-04-19 23:10:25 +0530

    Initial commit

---
+++ pingPong.js
@@ -0,0 +1,21 @@
+/*
+//  Ping Pong
+//  pingPong.js
+//  Created by Ashish Gogna on 4/19/16.
+*/
+
+//Global variables
+var windowWidth = $(window).width();
+var windowHeight = $(window).height();
+
+//The World
+var world = {
+    ctx:null,
+    fps:20,
+    themeColor: "#3B444B"
+}
+
+//Document onload function
+$(document).ready(function () {
+
+});
